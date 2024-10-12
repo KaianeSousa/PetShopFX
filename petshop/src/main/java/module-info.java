a99b409ab2953a01e40f5750e4046653525dc028
KaianeSousa <132933423+KaianeSousa@users.noreply.github.com>
2024-10-12 18:47:15 -0300

    Add files via upload

---
+++ petshop/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module org.examplebr.com.petshop.petshop {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
+
+    opens org.examplebr.com.petshop.petshop to javafx.fxml;
+    exports org.examplebr.com.petshop.petshop;
+}
